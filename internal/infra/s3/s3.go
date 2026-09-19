164a51e8e48484938045fcb861acc26d5a85d685
ChumbaJ <step130202@yandex.ru>
2026-09-19 21:54:31 +0800

    chore: s3

--- internal/infra/s3/s3.go
+++ internal/infra/s3/s3.go
@@ -3,14 +3,15 @@ package s3
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/smithy-go"
 	"github.com/google/uuid"
 )
 
@@ -20,25 +21,42 @@ type Storage struct {
 }
 
 func New(ctx context.Context, awsAK string, awsSK string, bucket string) *Storage {
-	cfg, err := config.LoadDefaultConfig(ctx,
-		config.WithRegion("us-east-1"),
-		config.WithCredentialsProvider(
-			credentials.NewStaticCredentialsProvider(awsAK, awsSK, ""),
-		),
-		config.WithBaseEndpoint("http://localhost:8333"),
+	sdkConfig, err := config.LoadDefaultConfig(ctx,
+		config.WithDefaultRegion("garage"),
+		config.WithBaseEndpoint("http://localhost:3900"),
 	)
 	if err != nil {
-		panic(err)
+		fmt.Println("Couldn't load default configuration. Have you set up your AWS account?")
+		fmt.Println(err)
+		return nil
 	}
 
-	client := s3.NewFromConfig(cfg, func(o *s3.Options) {
-		o.UsePathStyle = true
-		o.RequestChecksumCalculation = aws.RequestChecksumCalculationWhenRequired
-		o.ResponseChecksumValidation = aws.ResponseChecksumValidationWhenRequired
-	})
+	s3Client := s3.NewFromConfig(sdkConfig)
+	count := 10
+	fmt.Printf("Let's list up to %v buckets for your account.\n", count)
+	result, err := s3Client.ListBuckets(ctx, &s3.ListBucketsInput{})
+	if err != nil {
+		var ae smithy.APIError
+		if errors.As(err, &ae) && ae.ErrorCode() == "AccessDenied" {
+			fmt.Println("You don't have permission to list buckets for this account.")
+		} else {
+			fmt.Printf("Couldn't list buckets for your account. Here's why: %v\n", err)
+		}
+		return nil
+	}
+	if len(result.Buckets) == 0 {
+		fmt.Println("You don't have any buckets!")
+	} else {
+		if count > len(result.Buckets) {
+			count = len(result.Buckets)
+		}
+		for _, bucket := range result.Buckets[:count] {
+			fmt.Printf("\t%v\n", *bucket.Name)
+		}
+	}
 
 	return &Storage{
-		client: client,
+		client: s3Client,
 		Bucket: bucket,
 	}
 }
